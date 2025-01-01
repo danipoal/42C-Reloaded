2ea05033c68337d142a70da6d6bc3658ffadd85e
Daniel Alvarez del Reguero <danalvar@student.42barcelona.com>
2025-01-01 20:17:40 +0100

    sqrt, putstr and len

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danalvar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/01 17:33:32 by danalvar          #+#    #+#             */
+/*   Updated: 2025/01/01 20:09:19 by danalvar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+/*void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}*/
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_putstr("Hola qe tal");
+	return (0);
+}*/
